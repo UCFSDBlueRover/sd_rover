f2da7d55d51ed89e215159a57ea563c7c3fc9758
Wesley Fletcher <wesfletch@gmail.com>
2022-03-17 20:45:23 -0400

    adjusts speeds to not tip the robot

--- rover_sim/scripts/motors.py
+++ rover_sim/scripts/motors.py
@@ -32,16 +32,16 @@ def on_press(key):
 
     # modify twist message depending on which arrow key pressed    
     if( k == 'left' ):
-        twist.angular.z = -.4
+        twist.angular.z = -.15
         change_flag = True
     if( k == 'right' ):
-        twist.angular.z = .4
+        twist.angular.z = .15
         change_flag = True
     if( k == 'up' ):
-        twist.linear.x = .2
+        twist.linear.x = .1
         change_flag = True
     if( k == 'down' ):
-        twist.linear.x = -.2
+        twist.linear.x = -.1
         change_flag = True      
 
     # update published twist message
