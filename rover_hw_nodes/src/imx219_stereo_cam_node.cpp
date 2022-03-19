14e2df11fceab2db9f0caa623a5675f4ed3fcab8
Wesley Fletcher <wesfletch@gmail.com>
2022-03-18 20:35:49 -0400

    fixes to custom camera node

--- rover_hw_nodes/src/imx219_stereo_cam_node.cpp
+++ rover_hw_nodes/src/imx219_stereo_cam_node.cpp
@@ -10,6 +10,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <camera_info_manager/camera_info_manager.h>
 
 // integrated IMU
 extern "C"
@@ -21,6 +22,7 @@ extern "C"
 
 // ROS messages
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Vector3.h>
@@ -65,7 +67,6 @@ void buildImuMsg(IMU_ST_ANGLES_DATA stAngles, IMU_ST_SENSOR_DATA stGyroRawData,
     imuMsg->angular_velocity = angularVel;
 
     // populate angular velocity covariance
-
     boost::array<double, 9> angularVelCovariance = { 1.,0.,0.,
                                                      0.,1.,0.,
                                                      0.,0.,1.};
@@ -98,27 +99,43 @@ int main(int argc, char **argv)
     image_transport::ImageTransport it(n);
 
     // advertise our output topics
-    image_transport::Publisher im0_pub = it.advertise("/im0", 1);
-    image_transport::Publisher im1_pub = it.advertise("/im1", 1);
+    
+    // left and right images
+    image_transport::CameraPublisher im0_pub = it.advertiseCamera("/im0", 1, false);
+    image_transport::CameraPublisher im1_pub = it.advertiseCamera("/im1", 1, false);
+    
+    // left and right image info
+    const std::string cname_0 = "waveshare_imx219_left";
+    const std::string url_0 = "file:///home/jetson/catkin_ws/src/sd_rover/rover_hw_nodes/config/stereo_left.yaml";
+    camera_info_manager::CameraInfoManager im0_info(n, cname_0, url_0);
+    im0_info.loadCameraInfo(url_0);
+    const std::string cname_1 = "waveshare_imx219_right";
+    const std::string url_1 = "file:///home/jetson/catkin_ws/src/sd_rover/rover_hw_nodes/config/stereo_right.yaml";
+    camera_info_manager::CameraInfoManager im1_info(n, cname_1, url_1);
+    im0_info.loadCameraInfo(url_1);
+
+
+
+    // imu message
     ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("/imu", 10);
 
     // configure the IMU fields
     IMU_EN_SENSOR_TYPE enMotionSensorType;
-	IMU_ST_ANGLES_DATA stAngles;
-	IMU_ST_SENSOR_DATA stGyroRawData;
-	IMU_ST_SENSOR_DATA stAccelRawData;
-	IMU_ST_SENSOR_DATA stMagnRawData;
+    IMU_ST_ANGLES_DATA stAngles;
+    IMU_ST_SENSOR_DATA stGyroRawData;
+    IMU_ST_SENSOR_DATA stAccelRawData;
+    IMU_ST_SENSOR_DATA stMagnRawData;
     
     // initialize IMU data connection
     imuInit(&enMotionSensorType);
     if(IMU_EN_SENSOR_TYPE_ICM20948 == enMotionSensorType)
-	{
-		printf("Motion sersor is ICM-20948\n" );
-	}
-	else
-	{
-		printf("Motion sersor NULL\n");
-	}
+    {
+        printf("Motion sersor is ICM-20948\n" );
+    }
+    else
+    {
+        printf("Motion sersor NULL\n");
+    }
 
     // configure our cameras with nvarguscamerasrc (CSI connector) pipeline
     cv::VideoCapture cam0("nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=640, height=480, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink", cv::CAP_GSTREAMER);
@@ -154,13 +171,17 @@ int main(int argc, char **argv)
         // publish camera frames as images (if they're not empty)
         if (!frame0.empty())
         {
+            sensor_msgs::CameraInfo::Ptr cinfo_0(
+                new sensor_msgs::CameraInfo(im0_info.getCameraInfo()));
             msg0 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame0).toImageMsg();
-            im0_pub.publish(msg0);
+            im0_pub.publish(msg0, cinfo_0);
         }
         if (!frame1.empty())
         {
+            sensor_msgs::CameraInfo::Ptr cinfo_1(
+                new sensor_msgs::CameraInfo(im1_info.getCameraInfo()));
             msg1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame1).toImageMsg();
-            im1_pub.publish(msg1);
+            im1_pub.publish(msg1, cinfo_1);
         }
         cv::waitKey(1);
 
